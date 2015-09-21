92b6bbe866aa3f0a550c623031b8fb96f38c718c
Roberto Ortega <berto.ort@gmail.com>
2015-09-21 16:49:18 -0600

    deploying

--- main.go
+++ main.go
@@ -1,5 +1,73 @@
 package main
 
+import (
+	// "encoding/json"
+	// "fmt"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/mgo.v2"
+	// "gopkg.in/mgo.v2/bson"
+	// "io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+// Beer is the model for mongo
+type Beer struct {
+	Name    string
+	Brewery string
+	Style   string
+	Alcohol int
+	current bool
+}
+
+//Query is used to find the current beer
+type Query struct {
+	current bool
+}
+
 func main() {
+	lab := os.Getenv("MONGOLAB_URI")
+	db := os.Getenv("DATABASE_NAME")
+
+	session, err := mgo.Dial(lab)
+	col := session.DB(db).C("beers")
+	if err != nil {
+		panic(err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*.html")
+	r.Static("/public", "public")
+
+	r.GET("/", func(c *gin.Context) {
+		var beers []Beer
+		col.Find(nil).All(&beers)
+		current := col.Find(Query{current: true})
+		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
+			"beers":   beers,
+			"current": current,
+		})
+	})
+
+	r.POST("/addBeer", func(c *gin.Context) {
+		name := c.PostForm("name")
+		brewery := c.PostForm("brewery")
+		style := c.PostForm("style")
+		alcohol, _ := strconv.Atoi(c.PostForm("alcohol"))
+		err = col.Insert(&Beer{Name: name, Brewery: brewery, Style: style, Alcohol: alcohol, current: true})
+		if err != nil {
+			panic(err)
+		}
+		c.Redirect(http.StatusMovedPermanently, "/")
+	})
+
+	r.Run(":" + port)
 
 }
--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     function like() {
       $('.beer').addClass('rotate-left').delay(700).fadeOut(1);
       $('.like').show()
-      $('.display').css("background-image", "url(http://www.clker.com/cliparts/2/k/n/l/C/Q/transparent-green-checkmark-hi.png)")
+      $('.display').css("background-image", "url(http://cliparts.co/cliparts/dc9/rR6/dc9rR6ozi.svg)")
       setTimeout(function () {
         $('.like').hide()
         $('.beer').removeClass('rotate-left').fadeIn(1)
@@ -13,7 +13,7 @@ $(document).ready(function(){
     function dislike() {
       $('.beer').addClass('rotate-right').delay(700).fadeOut(1);
       $('.dislike').show()
-      $('.display').css("background-image", "url(http://www.clipartbest.com/cliparts/yTo/xdd/yToxddATE.png)")
+      $('.display').css("background-image", "url(http://www.clker.com/cliparts/1/1/9/2/12065738771352376078Arnoud999_Right_or_wrong_5.svg.med.png)")
       setTimeout(function () {
         $('.dislike').hide()
         $('.beer').removeClass('rotate-right').fadeIn(1)
@@ -37,6 +37,7 @@ $(document).ready(function(){
     });
 
     $('.likeButton').click(function () {
+      $(this).removeClass("dislikeButton")
       setTimeout(function () {
         $(this).addClass("likeButton")
       }.bind(this), 1500);
